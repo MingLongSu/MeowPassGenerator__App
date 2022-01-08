facf961fc5ff27ab82a520c495468c69d122705d
Jerry Su <BananaFrenzy31@gmail.com>
2022-01-08 15:03:01 -0500

    Fifth commit Implemented password generation system Project completed for now

--- client/src/components/PassGenInterface/PassGenInterface.js
+++ client/src/components/PassGenInterface/PassGenInterface.js
@@ -33,8 +33,71 @@ export default function PassGenInterface() {
     }
 
     const toggleLength = (e) => { 
-        const newLength = !isNaN(e.target.value) ? e.target.value : 0;
+        const newLength = !isNaN(e.target.value) ? parseInt(e.target.value) : 0;
         setLength(newLength);
+    }
+
+    const generatePassword = () => { 
+        const requirementsCount = isUpperCase + isLowerCase + isNumbers + isSymbols;
+        let newPassword = '';
+
+        if (requirementsCount === 0 || length === 0) { 
+            return newPassword;
+        }
+
+        // building an array of available requirements to meet
+        const availableRequirements = []
+
+        if (isUpperCase) { 
+            availableRequirements.push('u');
+        }
+
+        if (isLowerCase) { 
+            availableRequirements.push('l');
+        }
+
+        if (isNumbers) { 
+            availableRequirements.push('n');
+        }
+
+        if (isSymbols) { 
+            availableRequirements.push('s');
+        }
+
+        for (let i = 0; i < length; i++) { 
+            const elementOfPassword = availableRequirements[Math.floor(Math.random() * availableRequirements.length)]; 
+
+            if (elementOfPassword === 'u') {  
+                newPassword += getRandomUppercaseLetter();
+            }
+            else if (elementOfPassword === 'l') { 
+                newPassword += getRandomLowercaseLetter();
+            }
+            else if (elementOfPassword === 'n') { 
+                newPassword += getRandomNumber();
+            }
+            else if (elementOfPassword === 's') { 
+                newPassword += getRandomSymbol();
+            }
+        }
+        clipBoardRef.current.value = newPassword;
+    }
+
+    const getRandomUppercaseLetter = () => { 
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    }
+
+    const getRandomLowercaseLetter = () => { 
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    }
+
+    const getRandomNumber = () => {
+        return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    }
+
+    const getRandomSymbol = () => { 
+        const specialChars = "[$&+,:;=?@#|'<>.-^*()%!]"
+        return specialChars[Math.floor(Math.random() * 23)]
     }
 
     return (
@@ -60,7 +123,7 @@ export default function PassGenInterface() {
                 </div>
                 <div className='pass-gen-interface-body__row-2-body'>
                     <div className='row-2-body__generate-password-prompt-container'>
-                        <div className='generate-password-prompt-container__prompt'>
+                        <div onClick={ generatePassword } className='generate-password-prompt-container__prompt'>
                             <i className="fas fa-check"></i>
                         </div>
                     </div>
